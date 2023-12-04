dc723ba478a64147dff60480c514253027634c70
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2023-12-04 16:56:27 -0300

    style: StyledMain component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import GlobalStyle from "./GlobalStyle";
+import StyledMain from "./style";
 
 const Home = () => {
   return (
     <>
       <GlobalStyle />
-      <div>hello</div>
+      <StyledMain>hello</StyledMain>
     </>
   );
 };
---
+++ app/style.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledMain = styled.main`
+  display: flex;
+  flex-direction: row;
+  background-color: var(--White);
+  width: 728px;
+  height: 510px;
+  box-shadow: 4px 4px 12px var(--Pale-blue);
+  border-radius: 48px;
+`;
+
+export default StyledMain;
