eb361c5a17e94095f6bdef02bc6495fb40a7171b
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2023-12-04 16:51:40 -0300

    feat(layout): font and favicon configured

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,24 @@
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
+import type { NextFont } from "next/dist/compiled/@next/font";
+import { Hanken_Grotesk } from "next/font/google";
 import StyledComponentsRegistry from "./registry";
+
+const hankenGrotesk: NextFont = Hanken_Grotesk({
+  weight: ["500", "700", "800"],
+  style: "normal",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Results Summary Component",
+  icons: "/assets/favicon.png",
 };
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
-      <body>
+      <body className={hankenGrotesk.className}>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
