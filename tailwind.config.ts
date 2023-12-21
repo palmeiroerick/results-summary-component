49cb3d70d3c07675a0c836549b2686311eda9838
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2023-12-21 17:55:18 -0300

    chore: add prettier-plugin-tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,26 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: {
+    colors: {
+      "light-red": "#ff5757",
+      // /* Primary Colors */
+      // Lightred: "#ff5757",
+      // Orangeyyellow: "#ffb01f",
+      // Greenteal: "#00bd91",
+      // Cobaltblue: "#1125d4",
+
+      // /* Gradients Colors */
+      // Lightslate-blue: "#7857ff",
+      // Lightroyal-blue: "#2e2be8",
+      // Violetblue: "#4e21ca",
+      // Persianblue: "#2421ca00",
+
+      // /* Neutral Colors */
+      // White "#ffffff",
+      // Paleblue: "#ebf1ff",
+      // Lightlavender: "#c8c7ff",
+      // Darkgrayblue: "#303b59",
+    },
     extend: {},
   },
   plugins: [],
