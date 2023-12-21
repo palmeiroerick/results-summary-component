46be4abb5b22bb5462342e2abe9390d2ff81c0cb
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2023-12-21 18:27:34 -0300

    chore: config tailwindcss theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,25 +3,28 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: {
+    screens: {
+      sm: "375px",
+      lg: "1440px",
+    },
     colors: {
+      /* Primary Colors */
       "light-red": "#ff5757",
-      // /* Primary Colors */
-      // Lightred: "#ff5757",
-      // Orangeyyellow: "#ffb01f",
-      // Greenteal: "#00bd91",
-      // Cobaltblue: "#1125d4",
+      "orange-yellow": "#ffb01f",
+      "green-teal": "#00bd91",
+      "cobalt-blue": "#1125d4",
 
-      // /* Gradients Colors */
-      // Lightslate-blue: "#7857ff",
-      // Lightroyal-blue: "#2e2be8",
-      // Violetblue: "#4e21ca",
-      // Persianblue: "#2421ca00",
+      /* Gradients Colors */
+      "light-slate-blue": "#7857ff",
+      "light-royal-blue": "#2e2be8",
+      "violet-blue": "#4e21ca",
+      "persian-blue": "#2421ca00",
 
-      // /* Neutral Colors */
-      // White "#ffffff",
-      // Paleblue: "#ebf1ff",
-      // Lightlavender: "#c8c7ff",
-      // Darkgrayblue: "#303b59",
+      /* Neutral Colors */
+      white: "#ffffff",
+      "pale-blue": "#ebf1ff",
+      "light-lavender": "#c8c7ff",
+      "dark-gray-blue": "#303b59",
     },
     extend: {},
   },
