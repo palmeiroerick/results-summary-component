d21b8130ff67784a9862389242f52129d99a5bcb
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2023-12-22 16:16:35 -0300

    style: add style to score component

--- components/Score/index.tsx
+++ components/Score/index.tsx
@@ -2,9 +2,13 @@ import React from "react";
 
 const Score = () => {
   return (
-    <div>
-      <h1>76</h1>
-      <h4>of 100</h4>
+    <div className="my-4 flex h-48 w-48 flex-col items-center justify-center gap-2 rounded-full bg-gradient-to-b from-violet-blue to-persian-blue">
+      <h1 className="text-[5rem] font-extrabold leading-none text-pale-blue">
+        76
+      </h1>
+      <h4 className="text-lg font-bold text-light-lavender text-opacity-50">
+        of 100
+      </h4>
     </div>
   );
 };
