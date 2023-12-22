eb61cc7622dd3954bda1a8fe375b0c8758dfb0a8
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2023-12-22 17:28:00 -0300

    style: add style to Button component

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import type ButtonProps from "./type";
 
 const Button = ({ children }: ButtonProps) => {
-  return <button>{children}</button>;
+  return (
+    <button className="rounded-full bg-dark-gray-blue py-4 text-lg font-bold text-white">
+      {children}
+    </button>
+  );
 };
 
 export default Button;
