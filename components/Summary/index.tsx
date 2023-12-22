878edbaf0e7897b04c722aa9edf6397f6a6e4a90
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2023-12-22 18:37:20 -0300

    style: add desktop styles to Summary component

--- components/Summary/index.tsx
+++ components/Summary/index.tsx
@@ -5,8 +5,10 @@ import Button from "../Button";
 
 const Summary = () => {
   return (
-    <section className="flex flex-col gap-6 bg-white p-8">
-      <h3 className="text-lg font-bold text-dark-gray-blue">Summary</h3>
+    <section className="flex flex-col gap-6 bg-white p-8 md:w-1/2 md:justify-between md:rounded-[2.5rem]">
+      <h3 className="text-lg font-bold text-dark-gray-blue md:text-2xl">
+        Summary
+      </h3>
       <DataProvider>
         <CategoryList />
       </DataProvider>
