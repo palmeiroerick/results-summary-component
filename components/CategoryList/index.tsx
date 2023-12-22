f8966555e22dd7a2b017eaf609deae308a89ab06
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2023-12-22 17:28:21 -0300

    style: add style to CategoryList component

--- components/CategoryList/index.tsx
+++ components/CategoryList/index.tsx
@@ -1,18 +1,46 @@
 "use client";
 import { DataContext } from "@/contexts/DataContext";
+import Image from "next/image";
 import React, { useContext } from "react";
 
 const CategoryList = () => {
   const data = useContext(DataContext);
 
   return (
-    <ul>
-      {data.map(({ category, score }, index) => (
-        <li key={index}>
-          <h3>{category}</h3>
-          <h4>{score}</h4>
-        </li>
-      ))}
+    <ul className="flex flex-col gap-4">
+      {data.map(({ category, score, icon, color }, index) => {
+        const bgColors = {
+          "light-red": "bg-light-red/5",
+          "orange-yellow": "bg-orange-yellow/5",
+          "green-teal": "bg-green-teal/5",
+          "cobalt-blue": "bg-cobalt-blue/5",
+        };
+
+        const textColors = {
+          "light-red": "text-light-red",
+          "orange-yellow": "text-orange-yellow",
+          "green-teal": "text-green-teal",
+          "cobalt-blue": "text-cobalt-blue",
+        };
+
+        return (
+          <li
+            key={index}
+            className={`${bgColors[color]} flex flex-row justify-between rounded-xl p-4`}
+          >
+            <div className="flex flex-row gap-4">
+              <Image src={icon} alt="" width={20} height={20} />
+              <h3 className={`${textColors[color]} font-bold capitalize `}>
+                {category}
+              </h3>
+            </div>
+            <div className="flex flex-row gap-2 font-bold">
+              <h4 className="text-dark-gray-blue">{score}</h4>
+              <h4 className="text-dark-gray-blue opacity-50">/ 100</h4>
+            </div>
+          </li>
+        );
+      })}
     </ul>
   );
 };
