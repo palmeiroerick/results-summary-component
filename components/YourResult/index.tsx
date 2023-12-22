6870138584058c9c17cdef8c5ded8ca1e8a56957
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2023-12-22 16:17:05 -0300

    style: add style to YourResult component

--- components/YourResult/index.tsx
+++ components/YourResult/index.tsx
@@ -3,11 +3,11 @@ import Score from "../Score";
 
 const YourResult = () => {
   return (
-    <section>
-      <h3>Your Result</h3>
+    <section className="flex flex-col items-center gap-2 bg-gradient-to-b from-light-slate-blue to-light-royal-blue px-11 py-8">
+      <h3 className="text-xl font-bold text-light-lavender">Your Result</h3>
       <Score />
-      <h2>Grate</h2>
-      <p>
+      <h2 className="text-3xl font-bold text-pale-blue">Grate</h2>
+      <p className="text-center text-lg text-light-lavender">
         You scored higher than 65% of the people who have taken these tests.
       </p>
     </section>
