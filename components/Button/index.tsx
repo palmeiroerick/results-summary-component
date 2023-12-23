bb1e11963a9bb170394b18be14642998b522d0b3
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2023-12-23 09:50:37 -0300

    style: add hover effect to Button component

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -3,7 +3,7 @@ import type ButtonProps from "./type";
 
 const Button = ({ children }: ButtonProps) => {
   return (
-    <button className="rounded-full bg-dark-gray-blue py-4 text-lg font-bold text-white">
+    <button className="rounded-full bg-dark-gray-blue py-4 text-lg font-bold text-white hover:bg-gradient-to-b hover:from-light-slate-blue hover:to-light-royal-blue">
       {children}
     </button>
   );
